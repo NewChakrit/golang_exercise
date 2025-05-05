e96d0d23b0adca9ea95e169043c822cb0f4cb3fe
Chakrit <chakrit.t@arise.tech>
2025-05-05 21:33:03 +0700

    add 070

--- 070-for-range/main.go
+++ 070-for-range/main.go
@@ -4,8 +4,29 @@ import "fmt"
 
 func main() {
 	// for range loop
-	// data structures - slice
+	// -- data structures - slice
 
 	xi := []int{42, 43, 44, 45, 46, 47}
-	fmt.Println(xi)
+	for i, v := range xi {
+		fmt.Println("range over a slice", i, v)
+	}
+	// result
+	//range over a slice 0 42
+	//range over a slice 1 43
+	//range over a slice 2 44
+	//range over a slice 3 45
+	//range over a slice 4 46
+	//range over a slice 5 47
+
+	// -- data structures - map
+	m := map[string]int{
+		"James":       42,
+		"Monneypenny": 32,
+	}
+	for k, y := range m {
+		fmt.Println("ranging over a map", k, y)
+	}
+	// result
+	// ranging over a map James 42
+	//ranging over a map Monneypenny 32
 }
