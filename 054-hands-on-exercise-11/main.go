b208c666f93df29e30347e981781542b7103be66
Chakrit <chakrit.t@arise.tech>
2025-05-05 16:38:27 +0700

    add 054

---
+++ 054-hands-on-exercise-11/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+// Terminal
+// 1) go build main.go >> create main
+// 2) ./main >> Hello World!
+// 3) rm main >> main has been removed
+// 4) go build . >> create 054...
+// 5) ./054-hands-on-exercise-11 >> Hello World!
+// 6) rm 054-hands-on-exercise-11   >> 054-hands-on-exercise-11  has been removed
+// 7) go build ./... >> create 054...
