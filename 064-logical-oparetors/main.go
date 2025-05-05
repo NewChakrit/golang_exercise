8f2d16829c1d59befc5ed079c5a6399931b1d41e
Chakrit <chakrit.t@arise.tech>
2025-05-05 17:30:34 +0700

    add 064

---
+++ 064-logical-oparetors/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 40
+	y := 5
+
+	if x < 42 && y < 42 {
+		fmt.Println("both are less than the meaning of life")
+	}
+
+	if x > 30 || x < 42 {
+		fmt.Println("x is getting close to the meaning of life")
+	}
+
+	if x != 42 || y != 10 {
+		fmt.Println("x is not 42 & y is not 10")
+	}
+}
