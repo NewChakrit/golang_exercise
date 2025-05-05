77518b0580fb8c381e2084976ff62c00c27e31e9
Chakrit <chakrit.t@arise.tech>
2025-05-05 15:33:07 +0700

    add 045

--- 046-mod-code-depend-01/main.go
+++ 046-mod-code-depend-01/main.go
@@ -8,8 +8,11 @@ import (
 func main() {
 	s1 := puppy.Bark()
 	s2 := puppy.Barks()
-
-	// also like this
 	fmt.Println(s1)
 	fmt.Println(s2)
+
+	// also like this
+
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
 }
