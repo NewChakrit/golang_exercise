7d323935ebdd715bfe450f813423089ccd24ad46
Chakrit <chakrit.t@arise.tech>
2025-05-07 01:39:13 +0700

    add 078

---
+++ 078-hands-on-exercise-23/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	x := rand.Intn(11)
+	y := rand.Intn(11)
+
+	fmt.Printf("The value of x is %v\n", x)
+	fmt.Printf("The value of y is %v\n", y)
+
+	switch {
+	case x < 4 && y < 4:
+		fmt.Println("x and y are both less than 4")
+	case x > 6 && y > 6:
+		fmt.Println("x and y are both greater than 4")
+	case x >= 4 && y <= 6:
+		fmt.Println("x is greater than or equal to 4 and less than or equal 6")
+	case y != 5:
+		fmt.Println("y is not 5")
+	default:
+		fmt.Println("none of the previous cases were met")
+	}
+
+}
