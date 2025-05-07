e371ecd4a23f0aac09c560298cc40e62bb18979a
Chakrit <chakrit.t@arise.tech>
2025-05-07 22:44:29 +0700

    add 107

---
+++ 107-hands-on-exercise-43/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{}
+	a = append(a, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51)
+
+	for _, v := range a {
+		fmt.Printf("value: %v \t type: %T\n", v, v)
+	}
+}
