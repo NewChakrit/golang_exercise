19b5ce836bb2e07f5fb5017d25ba3a046d554e0b
Chakrit <chakrit.t@arise.tech>
2025-05-07 22:24:19 +0700

    add 104

--- 104-slice-internal-02/main.go
+++ 104-slice-internal-02/main.go
@@ -4,16 +4,18 @@ import "fmt"
 
 func main() {
 	a := []int{0, 1, 2, 3, 4, 5}
-	b := a
+	b := make([]int, 6)
+	copy(b, a)
 
 	fmt.Println("a ", a)
 	fmt.Println("b ", b)
 	fmt.Println("-----------")
 
 	a[0] = 7
+	b[5] = 10
 	fmt.Println("a ", a)
 	fmt.Println("b ", b)
 	fmt.Println("-----------")
 	//a  [7 1 2 3 4 5]
-	//b  [7 1 2 3 4 5]
+	//b  [0 1 2 3 4 10]
 }
