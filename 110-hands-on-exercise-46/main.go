bbb182e54c950a187c345bf742cbf1da61b1907c
Chakrit <chakrit.t@arise.tech>
2025-05-07 23:03:30 +0700

    add 110

--- 110-hands-on-exercise-46/main.go
+++ 110-hands-on-exercise-46/main.go
@@ -3,22 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
-	a = append(a, 52)
-	fmt.Printf("a: %v\n", a)
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	//y := x[6:] // [48 49 50 51]
+	//
+	//x = x[:3]
+	//x = append(x, y...)
 
-	a = append(a, 53, 54, 55)
-	fmt.Printf("a updated: %v\n", a)
+	x = append(x[:3], x[6:]...)
+	fmt.Println(x)
 
-	y := []int{56, 57, 58, 59, 60}
-
-	for _, v := range y {
-		a = append(a, v)
-	}
-	fmt.Printf("a + y: %v\n", a)
-
-	z := []int{61, 62, 63, 64}
-	a = append(a, z...)
-	fmt.Printf("a + z: %v\n", a)
-
+	// -------------
 }
