91ef3a54abefdf4a5d5b60e79dc31c050e09233e
Chakrit <chakrit.t@arise.tech>
2025-05-08 20:29:26 +0700

    add 114

---
+++ 114-map-for-range-over-a-map/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	am := map[string]int{
+		"Todd":   42,
+		"Henry":  16,
+		"Padget": 14,
+	}
+
+	fmt.Println(am)
+	fmt.Printf("%#v\n", am)
+
+	fmt.Println("------------")
+
+	bm := make(map[string]int)
+	bm["Lisa"] = 22
+	bm["Bob"] = 35
+	bm["George"] = 78
+
+	fmt.Println(bm)
+	fmt.Printf("%#v\n", bm)
+	fmt.Println(len(bm)) // 2
+
+	for k, v := range bm {
+		fmt.Printf("%v is %v years old.\n", k, v)
+	}
+
+	for _, v := range bm {
+		fmt.Println(v)
+	}
+
+	for k, _ := range bm {
+		fmt.Println(k)
+	}
+
+	for i := range bm {
+		fmt.Println(i) // Key
+	}
+
+}
