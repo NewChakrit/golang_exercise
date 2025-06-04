ebffa859dd0a566dc552c61ba64bcdafd303469e
Chakrit <imnew108@gmail.com>
2025-06-04 13:37:00 +0700

    add 010-00

--- 010-testing/05-benchmark/00-live/saying/saying_test.go
+++ 010-testing/05-benchmark/00-live/saying/saying_test.go
@@ -25,4 +25,12 @@ func BenchmarkGreet(b *testing.B) {
 }
 
 // Try cml:
-// go test -bench Greet || go test -bench . (to run all func)
+// 1. go test -bench Greet || go test -bench . (to run all func)
+// 2. go test -coverprofile <file name>
+// EX go test -coverprofile c.out:
+// ่คนี่คือ flag ที่สั่งให้ go test วัด Code Coverage และ บันทึกผลลัพธ์ลงในไฟล์
+// coverprofile: เป็นชื่อ flag ที่บ่งบอกว่าเราต้องการสร้าง "coverage profile"
+// c.out: คือชื่อไฟล์ที่คุณต้องการบันทึกผลลัพธ์ของ Code Coverage ลงไป คุณสามารถใช้ชื่อไฟล์อะไรก็ได้ที่คุณต้องการ (เช่น coverage.txt, coverage.out, etc.) แต่นิยมใช้ .out
+
+// 3. คำสั่ง go tool cover -html=<file name>
+//  เป็นคำสั่ง Go ที่ใช้ในการ สร้างรายงาน Code Coverage ในรูปแบบ HTML จากไฟล์ c.out ที่ได้จากการรัน Unit Tests ด้วย go test -coverprofile
